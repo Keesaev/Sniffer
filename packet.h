bc473df59f9459a75e1fa7805727e44039d7e0ca
keesaev <kostia.234@mail.ru>
2021-06-04 21:24:33 +0300

    Added demo

--- packet.h
+++ packet.h
@@ -10,10 +10,6 @@ public:
     Packet(){
 
     }
-    Packet(int n, QString sIp, QString dIp, QString p, QString l, QString f,QTime t) :
-        number(n), sourceIp(sIp), destIp(dIp), protocol(p), length(l), fullData(f), timestamp(t){
-
-    }
     int number;
     QString sourceIp, destIp, protocol, length, fullData;
     QTime timestamp;
